a8f91262f3bb8e6b50b378e9eb51663d0f8992cc
Florencia <69648398+florenciaferrigno@users.noreply.github.com>
2021-06-04 16:33:59 -0300

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 CFGPARMS_SSL="--prefix=/openssl"
-CFGPARMS_XERCES="--prefix=/xerces --host=m32c"
-CFGPARMS_JANSON="--prefix=/jansson --host=m32c"
+CFGPARMS_XERCES="--prefix=/xerces"
+CFGPARMS_JANSON="--prefix=/jansson"
 
+export CXX_FLAGS=-m32
+export CFLAGS=-32
 tar -xvf ECR-Dependencias/openssl-1.0.0e.tar.gz 
 cp -rf openssl-1.0.0e /openssl
 cd /openssl 
